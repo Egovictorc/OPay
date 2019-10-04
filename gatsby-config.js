aa0b25b8407960ac66c7aa121128106fd6ac847b
egovictor <egovictorc@outlook.com>
2019-10-04 10:37:58 +0100

    add project description

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `OPay`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `OPay ensures Fast, secured and Reliable Transactions`,
+    author: `@Egovictorc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
